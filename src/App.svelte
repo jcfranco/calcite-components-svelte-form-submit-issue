36db12bdfeda55232d9cf5ee536ad93b258ad9ce
Gavin Rehkemper <grehkemper@esri.com>
2021-02-25 14:30:25 -0600

    demo issue

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,46 @@
 <script>
-	export let name;
+  const onSubmitHandler = (evt) => {
+    console.log("SUBMIT!");
+    alert("SUBMIT!");
+  };
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+Expected: When you submit the form below it should call the form submit handler
+JavaScript (show an alert).
+<br /><br /><br />
+
+<form
+  on:submit|preventDefault={onSubmitHandler}
+  style="border: 1px solid gray; padding: 10px;"
+>
+  <calcite-label status="idle" scale="m">
+    First Name
+    <calcite-input
+      name="firstname"
+      type="text"
+      status="idle"
+      alignment="start"
+    />
+  </calcite-label>
+
+  this calcite submit submits the form traditionally instead of calling our
+  onSubmitHandler created using Svelte:
+  <calcite-button
+    appearance="solid"
+    color="blue"
+    scale="m"
+    dir="ltr"
+    alignment="center"
+    width="auto"
+    type="submit"
+  >
+    Submit
+  </calcite-button>
+
+  <br /><br />
+  this non-calcite one does work:
+  <button type="submit" value="Submit">Submit</button>
+</form>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
